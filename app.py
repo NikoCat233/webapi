0bddb4533375f8bf975f4c0628d145ca22019a22
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2025-05-06 21:29:05 +0800

    format plugin & add referer

--- app.py
+++ app.py
@@ -11,128 +11,148 @@ from urllib3.util.retry import Retry
 from fake_useragent import UserAgent
 from zoneinfo import ZoneInfo
 
-tz = ZoneInfo('Asia/Shanghai')
+tz = ZoneInfo("Asia/Shanghai")
 
 session = requests.Session()
 retry = Retry(total=30, backoff_factor=1)
 
 adapter = HTTPAdapter(max_retries=retry)
-session.mount('https://',adapter)
-session.mount('http://',adapter)
+session.mount("https://", adapter)
+session.mount("http://", adapter)
 
 app = FastAPI()
 
 scheduler = AsyncIOScheduler()
 
 banHistoryExample = {
-    "time":0,
-    "formated":"00:00:00",
-    "watchdog":False, # if the ban is from watchdog
-    "number":1,
+    "time": 0,
+    "formated": "00:00:00",
+    "watchdog": False,  # if the ban is from watchdog
+    "number": 1,
 }
 
 banHistory = []
 LockBanHistory = Lock()
 
 watchdog = {
-    "last_minute":0,
-    "last_day":0,
-    "total":-1,
+    "last_minute": 0,
+    "last_day": 0,
+    "total": -1,
 }
 
 staffHalfHourCalc = NumberManager()
 
 staff = {
-    "last_half_hour":0,
-    "last_day":0,
-    "total":-1,
+    "last_half_hour": 0,
+    "last_day": 0,
+    "total": -1,
 }
 
 lastUpdated = time.time()
 
-@scheduler.scheduled_job('interval', seconds=6,id='getBanData')
-async def getBanData():
-    global staff,watchdog,staffHalfHourCalc,banHistory,LockBanHistory,lastUpdated,tz
-    punishmentStats = session.get('https://api.plancke.io/hypixel/v1/punishmentStats',headers={
-        'User-Agent': UserAgent().random,
-    }).json()['record']
-    staff['last_day'] = punishmentStats['staff_rollingDaily']
-    watchdog['last_day'] = punishmentStats['watchdog_rollingDaily']
-    watchdog['last_minute'] = punishmentStats['watchdog_lastMinute']
 
-    if staff['total'] == -1 or watchdog['total'] == -1:
-        staff['total'] = punishmentStats['staff_total']
-        watchdog['total'] = punishmentStats['watchdog_total']
+@scheduler.scheduled_job("interval", seconds=6, id="getBanData")
+async def getBanData():
+    global staff, watchdog, staffHalfHourCalc, banHistory, LockBanHistory, lastUpdated, tz
+    punishmentStats = session.get(
+        "https://api.plancke.io/hypixel/v1/punishmentStats",
+        headers={
+            "User-Agent": UserAgent().random,
+            "Referer": "https://plancke.io/",
+            "Accept": "application/json",
+        },
+    ).json()["record"]
+    staff["last_day"] = punishmentStats["staff_rollingDaily"]
+    watchdog["last_day"] = punishmentStats["watchdog_rollingDaily"]
+    watchdog["last_minute"] = punishmentStats["watchdog_lastMinute"]
+
+    if staff["total"] == -1 or watchdog["total"] == -1:
+        staff["total"] = punishmentStats["staff_total"]
+        watchdog["total"] = punishmentStats["watchdog_total"]
         lastUpdated = time.time()
         return
 
-    wdiff = punishmentStats['watchdog_total'] - watchdog['total']
-    sdiff = punishmentStats['staff_total'] - staff['total']
+    wdiff = punishmentStats["watchdog_total"] - watchdog["total"]
+    sdiff = punishmentStats["staff_total"] - staff["total"]
 
     if wdiff <= 0 and sdiff <= 0:
-        staff['total'] = punishmentStats['staff_total']
-        watchdog['total'] = punishmentStats['watchdog_total']
+        staff["total"] = punishmentStats["staff_total"]
+        watchdog["total"] = punishmentStats["watchdog_total"]
         lastUpdated = time.time()
         return
 
     now = time.time()
-    ndatetime = datetime.fromtimestamp(now,tz=tz)
+    ndatetime = datetime.fromtimestamp(now, tz=tz)
 
     with LockBanHistory:
         while len(banHistory) > 10:
             banHistory.pop()
-        
+
         if wdiff > 0:
             data = banHistoryExample.copy()
-            data['time'] = now
-            data['watchdog'] = True
-            data['number'] = wdiff
-            data['formated'] = f'{ndatetime:%H:%M:%S}'
-            banHistory.insert(0,data)
+            data["time"] = now
+            data["watchdog"] = True
+            data["number"] = wdiff
+            data["formated"] = f"{ndatetime:%H:%M:%S}"
+            banHistory.insert(0, data)
 
         if sdiff > 0:
             data = banHistoryExample.copy()
-            data['time'] = now
-            data['watchdog'] = False
-            data['number'] = sdiff
-            data['formated'] = f'{ndatetime:%H:%M:%S}'
+            data["time"] = now
+            data["watchdog"] = False
+            data["number"] = sdiff
+            data["formated"] = f"{ndatetime:%H:%M:%S}"
             staffHalfHourCalc.add(sdiff)
-            banHistory.insert(0,data)
+            banHistory.insert(0, data)
 
-    staff['total'] = punishmentStats['staff_total']
-    watchdog['total'] = punishmentStats['watchdog_total']
+    staff["total"] = punishmentStats["staff_total"]
+    watchdog["total"] = punishmentStats["watchdog_total"]
     lastUpdated = time.time()
 
 
 # remove the number that is older than 30 minutes
-@scheduler.scheduled_job('interval', seconds=3,id='removeHalfHour')
+@scheduler.scheduled_job("interval", seconds=3, id="removeHalfHour")
 async def _():
     staffHalfHourCalc.remove()
-    staff['last_half_hour'] = staffHalfHourCalc.get_count()
+    staff["last_half_hour"] = staffHalfHourCalc.get_count()
+
 
 @app.on_event("startup")
 async def _():
     await getBanData()
     scheduler.start()
 
+
 @app.on_event("shutdown")
 async def _():
     scheduler.shutdown()
 
 
-@app.get('/')
+@app.get("/")
 async def _():
-    global staff,watchdog,banHistory,LockBanHistory,lastUpdated,tz
+    global staff, watchdog, banHistory, LockBanHistory, lastUpdated, tz
     with LockBanHistory:
-        return {'staff':staff,'watchdog':watchdog,'banHistory':banHistory,'lastUpdated':{'timestamp':lastUpdated,'formated':datetime.fromtimestamp(lastUpdated,tz=tz).strftime('%H:%M:%S')}}
+        return {
+            "staff": staff,
+            "watchdog": watchdog,
+            "banHistory": banHistory,
+            "lastUpdated": {
+                "timestamp": lastUpdated,
+                "formated": datetime.fromtimestamp(lastUpdated, tz=tz).strftime(
+                    "%H:%M:%S"
+                ),
+            },
+        }
+
 
 def getAgo(gtime):
-    nd = datetime.fromtimestamp(gtime,tz=tz)
-    return f'{nd:%H:%M:%S} {time_since(gtime)}'
+    nd = datetime.fromtimestamp(gtime, tz=tz)
+    return f"{nd:%H:%M:%S} {time_since(gtime)}"
 
-@app.get('/wdr')
+
+@app.get("/wdr")
 async def _():
-    global watchdog,staff,banHistory,LockBanHistory,lastUpdated
+    global watchdog, staff, banHistory, LockBanHistory, lastUpdated
     with LockBanHistory:
         list = f"""🐕🐕 Hypixel Ban Tracker 👮‍👮‍
 [🐕] 过去一分钟有 {watchdog['last_minute']} 人被狗咬了
@@ -144,18 +164,19 @@ async def _():
 上次更新: {getAgo(lastUpdated) }
 """
         if len(banHistory) == 0:
-            list += '无最近封禁'
+            list += "无最近封禁"
         else:
-            list += '最近封禁记录:\n'
+            list += "最近封禁记录:\n"
             for ban in banHistory:
                 list += f"[{'🐕' if ban['watchdog'] else '👮'}] [{ban['formated']}] banned {ban['number']} player.\n"
             list = list[:-1]
 
-    return {'wdr':list}
+    return {"wdr": list}
 
-@app.get('/wdr/raw')
+
+@app.get("/wdr/raw")
 async def _():
-    global watchdog,staff,banHistory,LockBanHistory,lastUpdated
+    global watchdog, staff, banHistory, LockBanHistory, lastUpdated
     with LockBanHistory:
         list = f"""🐕🐕 Hypixel Ban Tracker 👮‍👮‍
 [🐕] 过去一分钟有 {watchdog['last_minute']} 人被狗咬了
@@ -167,11 +188,11 @@ async def _():
 上次更新: {getAgo(lastUpdated) }
 """
         if len(banHistory) == 0:
-            list += '无最近封禁'
+            list += "无最近封禁"
         else:
-            list += '最近封禁记录:\n'
+            list += "最近封禁记录:\n"
             for ban in banHistory:
                 list += f"[{'🐕' if ban['watchdog'] else '👮'}] [{ban['formated']}] banned {ban['number']} player.\n"
             list = list[:-1]
 
-    return Response(content=list, media_type='text/plain')
+    return Response(content=list, media_type="text/plain")
--- numbermanager.py
+++ numbermanager.py
@@ -2,37 +2,38 @@ import time
 from collections import OrderedDict
 from threading import Lock
 
+
 class NumberManager:
     controlLock = Lock()
 
     def __init__(self):
         self.numbers = []
-    
+
     def add(self, number):
         """添加数字并记录当前时间戳"""
         current_time = time.time()
         with self.controlLock:
-            self.numbers.append({'number':number,'ctime':current_time})
-    
+            self.numbers.append({"number": number, "ctime": current_time})
+
     def remove(self):
         """删除30分钟前添加的数字"""
         current_time = time.time()
         thirty_min_ago = current_time - 30 * 60
-        
+
         to_delete = []
 
         for num in self.numbers:
-            ctime = num['ctime']
+            ctime = num["ctime"]
             if ctime < thirty_min_ago:
                 to_delete.append(num)
         with self.controlLock:
             for num in to_delete:
                 self.numbers.remove(num)
-    
+
     def get_count(self):
         """获取当前数字的总数"""
         count = 0
         with self.controlLock:
             for num in self.numbers:
-                count += num['number']
+                count += num["number"]
         return count
